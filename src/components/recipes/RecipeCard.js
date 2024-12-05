5a3e16b16de5d0fee504697ec4420e98dd20b505
LuisCondeEsnayra <luisesnayra@gmail.com>
2024-12-04 20:41:12 -0600

    update: card

--- src/components/recipes/RecipeCard.js
+++ src/components/recipes/RecipeCard.js
@@ -1,14 +1,22 @@
  
+import CIcon from "@coreui/icons-react";
 import classes from "./recipeCard.module.css";
+import { cilRestaurant } from "@coreui/icons";
 
 const RecipeCard = (props) => {
   const { name, image } = props.recipe;
 
   return (
-    <div onClick={props.handleShowMore}>
-      <h5>{name}</h5>
-      <img src={image} alt={name} className={`${classes.recipeImage}  `} />
+    <div onClick={props.handleShowMore} className={classes.card}>
+    <h5>{name}</h5>
+    <div className={`bg-success ${classes.midBar} text-center`}>
+      <div>
+        <CIcon className={classes.circleIcon} icon={cilRestaurant} size={"xxl"} />
+      </div>
     </div>
+    <img src={image} alt={name} className={`${classes.recipeImage}`} />
+  </div>
+  
   );
 };
 
